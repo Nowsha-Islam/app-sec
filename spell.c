ebf0f1a0f1a32449b8b7b0c34c7564bc6104f144
Nowsha Islam <nni209@nyu.edu>
2019-09-30 01:54:37 -0400

    adding travis test

--- spell.c
+++ spell.c
@@ -146,3 +146,13 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
     }
 };
  
+int main(int argc, char * argv[]) {
+	hashmap_t hashtable[HASH_SIZE];
+    FILE *fp = fopen(argv[1], "r");
+    load_dictionary(argv[2], hashtable);
+    char *misspelled[MAX_MISSPELLED];
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    fclose(fp);
+	printf("MISPELLED: %i\n",num_misspelled);
+	return 0;
+}
